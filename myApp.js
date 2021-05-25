fc27787139e90d9932eb05312bf4025f6d13eafb
caitlingbailey <>
2021-05-25 10:44:59 +0000

    Wrote database methods

--- myApp.js
+++ myApp.js
@@ -52,20 +52,47 @@ const findOneByFood = (food, done) => {
 
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data)
+  });
 };
+
 
 const findEditThenSave = (personId, done) => {
+  // Define the food to add
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  // Find the person by ID
+  Person.findById(personId, (err, person) => {
+    if (err) return console.log(err);
+
+    // Add the new food
+    person.favoriteFoods.push(foodToAdd);
+
+    // Save the updated person
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson)
+    });
+  });
 };
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName},
+    {age : ageToSet},
+    { new: true },
+    (err, updatedDoc) => {
+      if (err) return console.log(err);
+      done(null, updatedDoc)
+    }
+  );
 };
+
+
+
 
 const removeById = (personId, done) => {
   done(null /*, data*/);
